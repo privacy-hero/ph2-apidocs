33b3f87a526dcf3e5480c4fb1aa3e0f573da158b
Steven Johnson <sakurainds@gmail.com>
2020-05-08 06:25:29 +0700

    Allow easy regeneration of python client api

--- scripts/codegen.sh
+++ scripts/codegen.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 ./scripts/bundle.sh
 
+#docker run --user $(id -u):$(id -g) --rm -v ${PWD}:/local swaggerapi/swagger-codegen-cli-v3 generate \
+#    -i /local/API/PH2-API.oas3.json \
+#    -l python \
+#    -o /local/out/python-swagger
+
+docker pull openapitools/openapi-generator-cli
+
+rm -rf out/python-openapi
+
 docker run --user $(id -u):$(id -g) --rm -v \
     $PWD:/local openapitools/openapi-generator-cli generate \
     -i /local/API/PH2-API.oas3.json \
-    -g python-aiohttp \
-    -o /local/out/python-aiohttp
+    -g python \
+    -o /local/out/python-openapi
 
